7aa57c98fb558f680bad5a7ea4324ee419e32491
Kapilmeena1234 <104349187+Kapilmeena1234@users.noreply.github.com>
2022-05-06 23:50:18 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,26 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/user/Downloads/python2.py =================
+Length of the input string: 35
+egaugnal evitisnes esac a si nohtyP
+object oriented
+10
+Pythonisacasesensitivelanguage
+Hey, kapil meena Here! 
+My SID is 21107117 
+I am from Mechanical and my CGPA is 9.9
+8
+58
+50
+224 40
+14 0
+Enter a string:my name is
+Yes
+Enter triangle side 1:6
+Enter triangle side 2:6
+Enter triangle side 3:5
+Yes
+Enter first number:5
+Enter second number:6
+2
