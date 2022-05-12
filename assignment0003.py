c49c2d4d4f84360cdf7702f32cab41a97d078db0
Kapilmeena1234 <104349187+Kapilmeena1234@users.noreply.github.com>
2022-05-12 22:20:12 +0530

    Add files via upload

---
+++ assignment0003.py
@@ -0,0 +1,46 @@
+Enter the number to be converted to binary: 8
+0b1000
+Enter the expression 9
+9
+Enter the number n: 5
+Enter the number r: 7
+Enter the angle a: 4
+Enter the angle b: 6
+Enter the number x1: 8
+Enter the number x2: 2
+Enter the number y1: 7
+Enter the number y2: 9
+(3+4)(5) =  35
+n(n-1)/2 =  20.0
+4(pi)r^2 =  62.83185307179586
+sqrt(r(cosa)^2 + r(sinb)^2) =  1.8807607572751326
+y2-y1/x2-x1 =  -0.3333333333333333
+In the range(5)
+0
+1
+2
+3
+4
+In the range(3, 10)
+3
+4
+5
+6
+7
+8
+9
+In the range(4 ,13, 3)
+4
+7
+10
+In the range(15, 5, -2)
+15
+13
+11
+9
+7
+In the range(5, 3)
+Enter the number of hydrogen atoms: 5
+Enter the number of carbon atoms: 7
+Enter the number of oxygen atoms: 8
+217.11412
