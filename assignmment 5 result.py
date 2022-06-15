f656a23554010339e9a16c3c68f80b53f6ab9de3
Kapilmeena1234 <104349187+Kapilmeena1234@users.noreply.github.com>
2022-06-16 00:00:21 +0530

    Add files via upload

---
+++ assignmment 5 result.py
@@ -0,0 +1,110 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/kkkk.py ====
+Enter a string: lipak
+kapil
+Enter start range2
+Énter end range50
+Enter a number4
+4 is divisible by  4
+8 is divisible by  4
+12 is divisible by  4
+16 is divisible by  4
+20 is divisible by  4
+24 is divisible by  4
+28 is divisible by  4
+32 is divisible by  4
+36 is divisible by  4
+40 is divisible by  4
+44 is divisible by  4
+48 is divisible by  4
+Enter first side: 3
+Enter second side: 4
+Enter third side: 5
+The area of triangle is 6.0
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+Enter number of rows5
+A
+BC
+DEF
+GHIJ
+KLMNO
+Enter starting range8
+Enter ending range48
+11 is a prime number
+13 is a prime number
+17 is a prime number
+19 is a prime number
+23 is a prime number
+29 is a prime number
+31 is a prime number
+37 is a prime number
+41 is a prime number
+43 is a prime number
+47 is a prime number
+77
+154
+231
+308
+385
+462
+Enter an integer: 9
+Enter an integer: 5
+Enter an integer: -5
+Enter an integer: 5
+Enter an integer: 2
+Enter an integer: 7
+Enter an integer: 6
+Enter an integer: 5
+Enter an integer: 8
+Enter an integer: 3
+[9, 5, -5, 5, 2, 7, 6, 5, 8, 3]
+9 is a positive number 
+9 is an odd number 
+5 is a positive number 
+5 is an odd number 
+-5 is a negative number 
+-5 is an odd number 
+5 is a positive number 
+5 is an odd number 
+2 is a positive number 
+2 is an even number 
+7 is a positive number 
+7 is an odd number 
+6 is a positive number 
+6 is an even number 
+5 is a positive number 
+5 is an odd number 
+8 is a positive number 
+8 is an even number 
+3 is a positive number 
+3 is an odd number 
+9 occurs 1 times
+5 occurs 3 times
+-5 occurs 1 times
+5 occurs 3 times
+2 occurs 1 times
+7 occurs 1 times
+6 occurs 1 times
+5 occurs 3 times
+8 occurs 1 times
+3 occurs 1 times
+Enter total elements in the list4
+Enter a word run
+Enter a word row
+Enter a word enter
+Enter a word end
+run occurs 1 times
+row occurs 1 times
+enter occurs 1 times
+end occurs 1 times
